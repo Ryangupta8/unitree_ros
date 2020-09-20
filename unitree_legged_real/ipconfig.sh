fd7d388383516413a5652c8d468a0d88eedfe63e
Ryan Gupta <ryangupta8@gmail.com>
2020-09-20 11:48:44 -0500

    add minkyu's stuff

--- unitree_legged_real/ipconfig.sh
+++ unitree_legged_real/ipconfig.sh
@@ -3,6 +3,6 @@
 sudo ifconfig lo multicast
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
 
-sudo ifconfig enx3c8cf8ff943a down
-sudo ifconfig enx3c8cf8ff943a up 192.168.123.161 netmask 255.255.255.0 # 192.168.123.161
+sudo ifconfig eno1 down
+sudo ifconfig eno1 up 192.168.123.161 netmask 255.255.255.0 # 192.168.123.161
 
