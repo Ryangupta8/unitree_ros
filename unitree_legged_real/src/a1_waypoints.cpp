e628ce5c4e0d752b4a81dbcfbf05f3764061217d
Ryan Gupta <ryangupta8@gmail.com>
2020-10-21 15:11:40 -0500

    updates for working in mas

--- unitree_legged_real/src/a1_waypoints.cpp
+++ unitree_legged_real/src/a1_waypoints.cpp
@@ -41,14 +41,27 @@ int main(int argc, char *argv[])
     geometry_msgs::PoseStamped goal_msg;
 
     // Define the hard coded goal points
-    double goal_x[4]; double goal_y[4]; double goal_th[4];
-    goal_x[0] = 7.43; goal_y[0] = 55.13; goal_th[0] = -1.57;
+    double goal_x[12]; double goal_y[12]; double goal_th[12];
+    goal_x[0] = 7.43; goal_y[0] = 55.13; goal_th[0] = -1.57; // center of smaller living room
     //goal_x[1] = 7.44; goal_y[1] = 50.13; goal_th[1] = -1.57;
-    goal_x[1] = 7.54; goal_y[1] = 50.13; goal_th[1] = -1.57;
-    goal_x[2] = 7.43; goal_y[2] = 55.13; goal_th[2] = 1.57;
+    goal_x[1] = 7.43; goal_y[1] = 50.13; goal_th[1] = -1.57;// center of larger living room
+    goal_x[2] = 7.5; goal_y[2] = 45.3; goal_th[2] = 0; // the foorway to back of apt
+    goal_x[3] = 11.5; goal_y[3] = 45.75; goal_th[3] = 1.57; // back corner of apt closest to stairs
+    goal_x[4] = 11.49; goal_y[4] = 55.5; goal_th[4] = 0.0; // Next to door to hall from kicthen
+    goal_x[5] = 14.6; goal_y[5] = 55.9; goal_th[5] = -1.57;// in hallway facing towards drone cage
+    goal_x[6] = 14.5; goal_y[6] = 37.5; goal_th[6] = -0.3; // Near drone cage in hall 
+    goal_x[7] = 16.0; goal_y[7] = 30.25; goal_th[7] = -1.57; // outside drone cage door
+    goal_x[8] = 12.88; goal_y[8] = 32.0; goal_th[8] = 1.57; // in front of vicon control pc
+    goal_x[9] = 13.05; goal_y[9] = 39.7; goal_th[9] = 1.75; // Near the corner by stairs before turning
+    goal_x[10] = 12.0; goal_y[10] = 41.0; goal_th[10] = 3.13; // Same but after the turn
+    goal_x[11] = 4.5; goal_y[11] = 41.5; goal_th[11] = 1.57; // Back around to our desks
+    // goal_x[5] = 11.5; goal_y[5] = 61.; goal_th[5] = -1.57;// by robocup @home setup
+    //goal_x[6] = 7.4; goal_y[6] = 58.20; goal_th[6] = 3.13; // between table and wall
+    //goal_x[7] = 5.2; goal_y[7] = 59.9; goal_th[7] = -1.8; // out in the passageway where person would be 
+    // goal_x[2] = 7.43; goal_y[2] = 55.13; goal_th[2] = 1.57;
     //goal_x[3] = 5.70; goal_y[3] = 59.75; goal_th[3] = 3.14;
-    goal_x[3] = 5.40; goal_y[3] = 59.95; goal_th[3] = 3.13;
-    goal_x[4] = 5.0; goal_y[4] = 57.5; goal_th[4] = -1.57;
+    // goal_x[3] = 5.40; goal_y[3] = 59.95; goal_th[3] = 3.13;
+    // goal_x[4] = 5.0; goal_y[4] = 57.5; goal_th[4] = -1.57;
 
     geometry_msgs::Quaternion goal_quat, curr_quat; 
     int i = 0;
@@ -99,7 +112,7 @@ int main(int argc, char *argv[])
 			// Get the quaternion of the goal for PoseStamped
             goal_msg.header.stamp = current_time;
 		    goal_quat = tf::createQuaternionMsgFromYaw(goal_th[i]);
-            if(i >= 5){ros::shutdown();}
+            //if(i >= 5){ros::shutdown();}
 		    // Fill the Goal msg
 		    goal_msg.pose.position.x = goal_x[i]; goal_msg.pose.position.y = goal_y[i]; 
 		    goal_msg.pose.orientation = goal_quat;
@@ -110,7 +123,8 @@ int main(int argc, char *argv[])
 
 		last_time = current_time;
 
-        if(i >= 5){
+        cout << "i = " << i << endl;
+        if(i >= 12){
             ros::shutdown();
         }
 
